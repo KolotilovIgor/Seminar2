987bbfb4592f0f3bea8ce0db201fd7bb26e106bb
IgorK <gosha424@mail.ru>
2023-12-30 17:29:52 +0300

    Задача1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход
+//трёхзначное число и удаляет вторую цифру этого
+//числа.
+//Примеры
+//a = 256 => 26
+//a = 891 => 81
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+
+if (num>99 && num<1000) // && - и выполнение обоих условий; || - выболнение одного из условий
+{
+    int num1 = num/100;
+    int num3 = num % 10;
+
+    int resault = num1*10 + num3;
+    Console.WriteLine(resault);
+}
