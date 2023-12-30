8174d16144b1620d633ad4f82f17ed1ac9bfd3f9
IgorK <gosha424@mail.ru>
2023-12-30 17:59:00 +0300

    Задача2

--- Program.cs
+++ Program.cs
@@ -5,15 +5,34 @@
 //a = 256 => 26
 //a = 891 => 81
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число");
+//int num = Convert.ToInt32(Console.ReadLine());
 //int num2 = int.Parse(Console.ReadLine());
 
-if (num>99 && num<1000) // && - и выполнение обоих условий; || - выболнение одного из условий
-{
-    int num1 = num/100;
-    int num3 = num % 10;
+//if (num>99 && num<1000) // && - и выполнение обоих условий; || - выболнение одного из условий
+//{
+//    int num1 = num/100;
+//    int num3 = num % 10;
+//
+//    int resault = num1*10 + num3;
+//    Console.WriteLine(resault);
+//}
 
-    int resault = num1*10 + num3;
-    Console.WriteLine(resault);
+//Напишите программу, которая принимает на вход трёхзначное
+//число и возводит вторую цифру этого числа в степень, равную
+//третьей цифре.
+//Примеры
+//487 => 8^7 = 2 097 152
+//254 => 5^4 = 625
+//617 => 1
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int num2 = num/10 %10;
+int num3 = num % 10;
+int resault = 1;
+for (int i = 0; i <num3; i++)
+{
+    resault =  resault * num2;
 }
+Console.WriteLine(resault);
